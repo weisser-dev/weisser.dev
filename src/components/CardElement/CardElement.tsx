49a3ae8b45ce93c05989aba54ca5149e268be560
Erik <erik.weisser@gmail.com>
2024-02-12 16:32:03 +0100

    some fancy logic

--- src/components/CardElement/CardElement.tsx
+++ src/components/CardElement/CardElement.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styles from './CardElement.module.css';
 import IconElement from '../IconElement/IconElement';
 
@@ -10,10 +10,13 @@ interface CardElementProps {
 }
 
 const CardElement: React.FC<CardElementProps> = ({title, description, href, icon}) => {
-  return (<div className={styles.cardElement}>
+  const [isHovered, setIsHovered] = useState(false);
+
+  return (<div className={styles.cardElement} onMouseEnter={() => setIsHovered(true)}
+               onMouseLeave={() => setIsHovered(false)}>
       <a href={href} target="_blank" rel="noopener noreferrer" aria-label={title}>
         <div className={styles.cardElementIcon}>
-          <IconElement icon={icon} title={title}/>
+          <IconElement isHovered={isHovered} icon={icon} title={title}/>
         </div>
         <div>
           <h5 className={styles.cardTitle}>{title}</h5>
--- src/components/IconElement/IconElement.tsx
+++ src/components/IconElement/IconElement.tsx
@@ -1,16 +1,35 @@
 import React from 'react';
+import styles from './IconElement.module.css';
 
 interface IconElementProps {
   icon: string;
   title: string;
+  isHovered?: boolean;
 }
 
-const IconElement: React.FC<IconElementProps> = ({icon, title}) => {
+const IconElement: React.FC<IconElementProps> = ({isHovered,icon, title}) => {
   const isFontAwesomeIcon = icon.startsWith('fa-') || icon.startsWith('fab ');
+  const isSVG = icon.endsWith('.svg'); // Simple check for SVG files
 
-  return (<div className="icon-element">
-      {isFontAwesomeIcon ? (<i className={`fas ${icon}`} title={title}></i>) : (<img src={icon} alt={title}/>)}
-    </div>);
+  // Prepare your SVG URLs
+  const defaultSVG = `url(${icon})`; // Path to your default SVG
+  const hoverSVG = `url(${icon}_hover.svg)`; // Path to your SVG for the hover state, assuming you have a way to differentiate it
+  console.log(hoverSVG);
+
+  return (
+    <div className="icon-element">
+      {isFontAwesomeIcon ? (
+        <i className={`fas ${icon}`} title={title}></i>
+      ) : isSVG ? (
+        <div
+          className={styles.iconImage}
+          style={{ backgroundImage: isHovered ? hoverSVG : defaultSVG }}
+        ></div>
+      ) : (
+        <img className={styles.iconImage} src={icon} alt={title} />
+      )}
+    </div>
+  );
 };
 
 export default IconElement;
